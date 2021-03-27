d755062d03bc184278a5e78dcb2f18eba548b601
chao <quchao@seas.upenn.edu>
2021-03-27 14:51:00 -0400

    move most of the template definition to cpp file so only instantiate once

--- map_plan/mpl/include/mpl_planner/planner_base.h
+++ map_plan/mpl/include/mpl_planner/planner_base.h
@@ -17,6 +17,10 @@ template <int Dim>
 class PlannerBase {
  public:
   using Coord = Waypoint<Dim>;
+  using EnvBaseD = EnvBase<Dim>;
+  using PrimitiveD = Primitive<Dim>;
+  using TrajectoryD = Trajectory<Dim>;
+  using StateSpaceD = StateSpace<Dim>;
 
   /// Simple constructor
   PlannerBase(bool verbose = false) : planner_verbose_(verbose) {}
@@ -25,135 +29,31 @@ class PlannerBase {
   bool initialized() { return !(ss_ptr_ == nullptr); }
 
   /// Get optimal trajectory
-  Trajectory<Dim> getTraj() const { return traj_; }
+  TrajectoryD getTraj() const { return traj_; }
   /// Get expanded collision free primitives
-  vec_E<Primitive<Dim>> getValidPrimitives() const {
-    vec_E<Primitive<Dim>> prs;
-    for (const auto &it : ss_ptr_->hm_) {
-      if (it.second && !it.second->pred_coord.empty()) {
-        for (unsigned int i = 0; i < it.second->pred_coord.size(); i++) {
-          Coord key = it.second->pred_coord[i];
-          // if(!ss_ptr_->hm_[key] ||
-          // std::isinf(it.second->pred_action_cost[i]))
-          if (std::isinf(it.second->pred_action_cost[i])) continue;
-          Primitive<Dim> pr;
-          env_->forward_action(ss_ptr_->hm_[key]->coord,
-                               it.second->pred_action_id[i], pr);
-          prs.push_back(pr);
-        }
-      }
-    }
-
-    if (planner_verbose_)
-      printf("number of states in hm: %zu, number of valid prs: %zu\n",
-             ss_ptr_->hm_.size(), prs.size());
-
-    return prs;
-  }
-
+  vec_E<PrimitiveD> getValidPrimitives() const;
   /// Get expanded primitives
-  vec_E<Primitive<Dim>> getAllPrimitives() const {
-    vec_E<Primitive<Dim>> prs;
-    for (const auto &it : ss_ptr_->hm_) {
-      if (it.second && !it.second->pred_coord.empty()) {
-        for (unsigned int i = 0; i < it.second->pred_coord.size(); i++) {
-          Coord key = it.second->pred_coord[i];
-          Primitive<Dim> pr;
-          env_->forward_action(key, it.second->pred_action_id[i], pr);
-          prs.push_back(pr);
-        }
-      }
-    }
-
-    if (planner_verbose_)
-      printf(
-          "getAllPrimitives number of states in hm: %zu, number of prs: %zu\n",
-          ss_ptr_->hm_.size(), prs.size());
-
-    return prs;
-  }
-
+  vec_E<PrimitiveD> getAllPrimitives() const;
   /// Get points in open set
-  vec_Vecf<Dim> getOpenSet() const {
-    vec_Vecf<Dim> ps;
-    for (const auto &it : ss_ptr_->pq_) ps.push_back(it.second->coord.pos);
-    return ps;
-  }
-
+  vec_Vecf<Dim> getOpenSet() const;
   /// Get points in close set
-  vec_Vecf<Dim> getCloseSet() const {
-    vec_Vecf<Dim> ps;
-    for (const auto &it : ss_ptr_->hm_) {
-      if (it.second && it.second->iterationclosed)
-        ps.push_back(it.second->coord.pos);
-    }
-    return ps;
-  }
-
+  vec_Vecf<Dim> getCloseSet() const;
   /// Get points neither in open nor close set
-  vec_Vecf<Dim> getNullSet() const {
-    vec_Vecf<Dim> ps;
-    for (const auto &it : ss_ptr_->hm_) {
-      if (it.second && !it.second->iterationopened)
-        ps.push_back(it.second->coord.pos);
-    }
-    return ps;
-  }
-
+  vec_Vecf<Dim> getNullSet() const;
   /// Get points at certain state
-  vec_Vecf<Dim> getStates(const Coord &state) const {
-    vec_Vecf<Dim> ps;
-    vec_Vecf<Dim> vels;
-    vec_Vecf<Dim> accs;
-    for (const auto &it : ss_ptr_->hm_) {
-      if (it.second) {
-        auto coord = it.second->coord;
-        bool add = true;
-        if (state.use_vel && (state.vel - coord.vel).norm() > 1e-3) add = false;
-        if (state.use_acc && (state.acc - coord.acc).norm() > 1e-3) add = false;
-        if (state.use_jrk && (state.jrk - coord.jrk).norm() > 1e-3) add = false;
-        if (add) {
-          // std::cout << "add pos: " << coord.pos.transpose() <<
-          //  " vel: " << coord.vel.transpose() <<
-          //  " acc: " << coord.acc.transpose() << std::endl;
-          ps.push_back(coord.pos);
-          bool new_vel = true;
-          for (const auto &it : vels) {
-            if ((it - coord.vel).norm() < 1e-3) {
-              new_vel = false;
-              break;
-            }
-          }
-
-          if (new_vel) vels.push_back(coord.vel);
-        }
-      }
-    }
-
-    for (const auto &it : vels)
-      std::cout << "vel: " << it.transpose() << std::endl;
-    std::cout << "=========================" << std::endl;
-    return ps;
-  }
-
+  vec_Vecf<Dim> getStates(const Coord &state) const;
   /// Get expanded nodes, for A* it should be the same as the close set
   vec_Vecf<Dim> getExpandedNodes() const { return env_->expanded_nodes_; }
-
   /// Get expanded edges, for A* it should be the same as the close set
-  vec_E<Primitive<Dim>> getExpandedEdges() const {
-    return env_->expanded_edges_;
-  }
-
+  vec_E<PrimitiveD> getExpandedEdges() const { return env_->expanded_edges_; }
   /// Get number of expanded nodes
   int getExpandedNum() const { return ss_ptr_->expand_iteration_; }
-
   /**
    * @brief Prune state space
    * @param time_step set the root of state space to be the waypoint on the best
    * trajectory at best_child_[time_step]
    */
   void getSubStateSpace(int time_step) { ss_ptr_->getSubStateSpace(time_step); }
-
   /// Get trajectory total cost
   decimal_t getTrajCost() const { return traj_cost_; }
 
@@ -161,108 +61,39 @@ class PlannerBase {
   void checkValidation() { ss_ptr_->checkValidation(ss_ptr_->hm_); }
 
   /// Reset state space
-  void reset() {
-    ss_ptr_ = nullptr;
-    traj_ = Trajectory<Dim>();
-  }
+  void reset();
 
   /// Set max vel in each axis
-  void setLPAstar(bool use_lpastar) {
-    use_lpastar_ = use_lpastar;
-    if (use_lpastar_)
-      printf("[PlannerBase] use Lifelong Planning A*\n");
-    else
-      printf("[PlannerBase] use normal A*\n");
-  }
-
+  void setLPAstar(bool use_lpastar);
   /// Set max vel in each axis
-  void setVmax(decimal_t v) {
-    env_->set_v_max(v);
-    if (planner_verbose_) printf("[PlannerBase] set v_max: %f\n", v);
-  }
-
+  void setVmax(decimal_t v);
   /// Set max acc in each axis
-  void setAmax(decimal_t a) {
-    env_->set_a_max(a);
-    if (planner_verbose_) printf("[PlannerBase] set a_max: %f\n", a);
-  }
-
+  void setAmax(decimal_t a);
   /// Set max jerk in each axis
-  void setJmax(decimal_t j) {
-    env_->set_j_max(j);
-    if (planner_verbose_) printf("[PlannerBase] set j_max: %f\n", j);
-  }
-
+  void setJmax(decimal_t j);
   /// Set max jerk in each axis
-  void setYawmax(decimal_t yaw) {
-    env_->set_yaw_max(yaw);
-    if (planner_verbose_) printf("[PlannerBase] set yaw_max: %f\n", yaw);
-  }
-
+  void setYawmax(decimal_t yaw);
   /// Set max time step to explore
-  void setTmax(decimal_t t) {
-    env_->set_t_max(t);
-    if (planner_verbose_) printf("[PlannerBase] set max time: %f\n", t);
-  }
-
+  void setTmax(decimal_t t);
   /// Set dt for each primitive
-  void setDt(decimal_t dt) {
-    env_->set_dt(dt);
-    if (planner_verbose_) printf("[PlannerBase] set dt: %f\n", dt);
-  }
-
+  void setDt(decimal_t dt);
   /// Set weight for cost in time
-  void setW(decimal_t w) {
-    env_->set_w(w);
-    if (planner_verbose_) printf("[PlannerBase] set w: %f\n", w);
-  }
-
+  void setW(decimal_t w);
   /// Set weight for cost in time
-  void setWyaw(decimal_t w) {
-    env_->set_wyaw(w);
-    if (planner_verbose_) printf("[PlannerBase] set wyaw: %f\n", w);
-  }
-
+  void setWyaw(decimal_t w);
   /// Set greedy searching param
-  void setEpsilon(decimal_t eps) {
-    epsilon_ = eps;
-    if (planner_verbose_) printf("[PlannerBase] set epsilon: %f\n", epsilon_);
-  }
-
+  void setEpsilon(decimal_t eps);
   /// Calculate heuristic using dynamics
-  void setHeurIgnoreDynamics(bool ignore) {
-    env_->set_heur_ignore_dynamics(ignore);
-    if (planner_verbose_)
-      printf("[PlannerBase] set heur_ignore_dynamics: %d\n", ignore);
-  }
-
+  void setHeurIgnoreDynamics(bool ignore);
   /// Set max number of expansion
-  void setMaxNum(int num) {
-    max_num_ = num;
-    if (planner_verbose_) printf("[PlannerBase] set max num: %d\n", max_num_);
-  }
-
+  void setMaxNum(int num);
   /// Set U
-  void setU(const vec_E<VecDf> &U) { env_->set_u(U); }
-
+  void setU(const vec_E<VecDf> &U);
   /// Set prior trajectory
-  void setPriorTrajectory(const Trajectory<Dim> &traj) {
-    env_->set_prior_trajectory(traj);
-    if (planner_verbose_) printf("[PlannerBase] set prior trajectory\n");
-  }
-
+  void setPriorTrajectory(const TrajectoryD &traj);
   /// Set tolerance in geometric and dynamic spaces
   void setTol(decimal_t tol_pos, decimal_t tol_vel = -1,
-              decimal_t tol_acc = -1) {
-    env_->set_tol_pos(tol_pos);
-    env_->set_tol_vel(tol_vel);
-    env_->set_tol_acc(tol_acc);
-    if (planner_verbose_) {
-      printf("[PlannerBase] set tol_pos: %f\n", tol_pos);
-      printf("[PlannerBase] set tol_vel: %f\n", tol_vel);
-      printf("[PlannerBase] set tol_acc: %f\n", tol_acc);
-    }
-  }
+              decimal_t tol_acc = -1);
 
   /**
    * @brief Planning thread
@@ -272,65 +103,15 @@ class PlannerBase {
    * The goal waypoint is the center of the goal region, the planner cannot find
    * the trajectory hits the exact goal state due to discretization
    */
-  bool plan(const Coord &start, const Coord &goal) {
-    if (planner_verbose_) {
-      start.print("Start:");
-      goal.print("Goal:");
-
-      env_->info();
-    }
-
-    if (!env_->is_free(start.pos)) {
-      printf(ANSI_COLOR_RED "[PlannerBase] start is not free!" ANSI_COLOR_RESET
-                            "\n");
-      return false;
-    }
-
-    std::unique_ptr<MPL::GraphSearch<Dim>> planner_ptr(
-        new MPL::GraphSearch<Dim>(planner_verbose_));
-
-    // If use A*, reset the state space
-    if (!use_lpastar_)
-      ss_ptr_.reset(new MPL::StateSpace<Dim>(epsilon_));
-    else {
-      // If use LPA*, reset the state space only at the initial planning
-      if (!initialized()) {
-        if (planner_verbose_)
-          printf(ANSI_COLOR_CYAN
-                 "[PlannerBase] reset planner state space!" ANSI_COLOR_RESET
-                 "\n");
-        ss_ptr_.reset(new MPL::StateSpace<Dim>(epsilon_));
-      }
-    }
-
-    env_->set_goal(goal);
-
-    env_->expanded_nodes_.clear();
-    env_->expanded_edges_.clear();
-
-    ss_ptr_->dt_ = env_->get_dt();
-    if (use_lpastar_)
-      traj_cost_ = planner_ptr->LPAstar(start, env_, ss_ptr_, traj_, max_num_);
-    else
-      traj_cost_ = planner_ptr->Astar(start, env_, ss_ptr_, traj_, max_num_);
-
-    if (std::isinf(traj_cost_)) {
-      if (planner_verbose_)
-        printf(ANSI_COLOR_RED
-               "[PlannerBase] Cannot find a traj!" ANSI_COLOR_RESET "\n");
-      return false;
-    }
-
-    return true;
-  }
+  bool plan(const Coord &start, const Coord &goal);
 
  protected:
   /// Environment class
-  std::shared_ptr<MPL::EnvBase<Dim>> env_;
+  std::shared_ptr<EnvBaseD> env_;
   /// Planner workspace
-  std::shared_ptr<MPL::StateSpace<Dim>> ss_ptr_;
+  std::shared_ptr<StateSpaceD> ss_ptr_;
   /// Optimal trajectory
-  Trajectory<Dim> traj_;
+  TrajectoryD traj_;
   /// Total cost of final trajectory
   decimal_t traj_cost_;
   /// Greedy searching parameter
---
+++ map_plan/mpl/src/planner_base.cpp
@@ -0,0 +1,279 @@
+#include "mpl_planner/planner_base.h"
+
+namespace MPL {
+
+template <int Dim>
+auto PlannerBase<Dim>::getValidPrimitives() const -> vec_E<PrimitiveD> {
+  vec_E<PrimitiveD> prs;
+  for (const auto &it : ss_ptr_->hm_) {
+    if (it.second && !it.second->pred_coord.empty()) {
+      for (unsigned int i = 0; i < it.second->pred_coord.size(); i++) {
+        Coord key = it.second->pred_coord[i];
+        // if(!ss_ptr_->hm_[key] ||
+        // std::isinf(it.second->pred_action_cost[i]))
+        if (std::isinf(it.second->pred_action_cost[i])) continue;
+        Primitive<Dim> pr;
+        env_->forward_action(ss_ptr_->hm_[key]->coord,
+                             it.second->pred_action_id[i], pr);
+        prs.push_back(pr);
+      }
+    }
+  }
+
+  if (planner_verbose_)
+    printf("number of states in hm: %zu, number of valid prs: %zu\n",
+           ss_ptr_->hm_.size(), prs.size());
+
+  return prs;
+}
+
+template <int Dim>
+auto PlannerBase<Dim>::getAllPrimitives() const -> vec_E<PrimitiveD> {
+  vec_E<PrimitiveD> prs;
+  for (const auto &it : ss_ptr_->hm_) {
+    if (it.second && !it.second->pred_coord.empty()) {
+      for (unsigned int i = 0; i < it.second->pred_coord.size(); i++) {
+        Coord key = it.second->pred_coord[i];
+        PrimitiveD pr;
+        env_->forward_action(key, it.second->pred_action_id[i], pr);
+        prs.push_back(pr);
+      }
+    }
+  }
+
+  if (planner_verbose_)
+    printf("getAllPrimitives number of states in hm: %zu, number of prs: %zu\n",
+           ss_ptr_->hm_.size(), prs.size());
+
+  return prs;
+}
+
+template <int Dim>
+vec_Vecf<Dim> PlannerBase<Dim>::getOpenSet() const {
+  vec_Vecf<Dim> ps;
+  for (const auto &it : ss_ptr_->pq_) {
+    ps.push_back(it.second->coord.pos);
+  }
+  return ps;
+}
+
+template <int Dim>
+vec_Vecf<Dim> PlannerBase<Dim>::getCloseSet() const {
+  vec_Vecf<Dim> ps;
+  for (const auto &it : ss_ptr_->hm_) {
+    if (it.second && it.second->iterationclosed)
+      ps.push_back(it.second->coord.pos);
+  }
+  return ps;
+}
+
+template <int Dim>
+vec_Vecf<Dim> PlannerBase<Dim>::getNullSet() const {
+  vec_Vecf<Dim> ps;
+  for (const auto &it : ss_ptr_->hm_) {
+    if (it.second && !it.second->iterationopened)
+      ps.push_back(it.second->coord.pos);
+  }
+  return ps;
+}
+
+template <int Dim>
+vec_Vecf<Dim> PlannerBase<Dim>::getStates(const Coord &state) const {
+  vec_Vecf<Dim> ps;
+  vec_Vecf<Dim> vels;
+  vec_Vecf<Dim> accs;
+  for (const auto &it : ss_ptr_->hm_) {
+    if (it.second) {
+      auto coord = it.second->coord;
+      bool add = true;
+      if (state.use_vel && (state.vel - coord.vel).norm() > 1e-3) add = false;
+      if (state.use_acc && (state.acc - coord.acc).norm() > 1e-3) add = false;
+      if (state.use_jrk && (state.jrk - coord.jrk).norm() > 1e-3) add = false;
+      if (add) {
+        // std::cout << "add pos: " << coord.pos.transpose() <<
+        //  " vel: " << coord.vel.transpose() <<
+        //  " acc: " << coord.acc.transpose() << std::endl;
+        ps.push_back(coord.pos);
+        bool new_vel = true;
+        for (const auto &it : vels) {
+          if ((it - coord.vel).norm() < 1e-3) {
+            new_vel = false;
+            break;
+          }
+        }
+
+        if (new_vel) vels.push_back(coord.vel);
+      }
+    }
+  }
+
+  for (const auto &it : vels)
+    std::cout << "vel: " << it.transpose() << std::endl;
+  std::cout << "=========================" << std::endl;
+  return ps;
+}
+
+template <int Dim>
+void PlannerBase<Dim>::reset() {
+  ss_ptr_ = nullptr;
+  traj_ = TrajectoryD();
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setLPAstar(bool use_lpastar) {
+  use_lpastar_ = use_lpastar;
+  if (use_lpastar_)
+    printf("[PlannerBase] use Lifelong Planning A*\n");
+  else
+    printf("[PlannerBase] use normal A*\n");
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setVmax(decimal_t v) {
+  env_->set_v_max(v);
+  if (planner_verbose_) printf("[PlannerBase] set v_max: %f\n", v);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setAmax(decimal_t a) {
+  env_->set_a_max(a);
+  if (planner_verbose_) printf("[PlannerBase] set a_max: %f\n", a);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setJmax(decimal_t j) {
+  env_->set_j_max(j);
+  if (planner_verbose_) printf("[PlannerBase] set j_max: %f\n", j);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setYawmax(decimal_t yaw) {
+  env_->set_yaw_max(yaw);
+  if (planner_verbose_) printf("[PlannerBase] set yaw_max: %f\n", yaw);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setTmax(decimal_t t) {
+  env_->set_t_max(t);
+  if (planner_verbose_) printf("[PlannerBase] set max time: %f\n", t);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setDt(decimal_t dt) {
+  env_->set_dt(dt);
+  if (planner_verbose_) printf("[PlannerBase] set dt: %f\n", dt);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setW(decimal_t w) {
+  env_->set_w(w);
+  if (planner_verbose_) printf("[PlannerBase] set w: %f\n", w);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setWyaw(decimal_t w) {
+  env_->set_wyaw(w);
+  if (planner_verbose_) printf("[PlannerBase] set wyaw: %f\n", w);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setEpsilon(decimal_t eps) {
+  epsilon_ = eps;
+  if (planner_verbose_) printf("[PlannerBase] set epsilon: %f\n", epsilon_);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setHeurIgnoreDynamics(bool ignore) {
+  env_->set_heur_ignore_dynamics(ignore);
+  if (planner_verbose_)
+    printf("[PlannerBase] set heur_ignore_dynamics: %d\n", ignore);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setMaxNum(int num) {
+  max_num_ = num;
+  if (planner_verbose_) printf("[PlannerBase] set max num: %d\n", max_num_);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setU(const vec_E<VecDf> &U) {
+  env_->set_u(U);
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setPriorTrajectory(const TrajectoryD &traj) {
+  env_->set_prior_trajectory(traj);
+  if (planner_verbose_) printf("[PlannerBase] set prior trajectory\n");
+}
+
+template <int Dim>
+void PlannerBase<Dim>::setTol(decimal_t tol_pos, decimal_t tol_vel,
+                              decimal_t tol_acc) {
+  env_->set_tol_pos(tol_pos);
+  env_->set_tol_vel(tol_vel);
+  env_->set_tol_acc(tol_acc);
+  if (planner_verbose_) {
+    printf("[PlannerBase] set tol_pos: %f\n", tol_pos);
+    printf("[PlannerBase] set tol_vel: %f\n", tol_vel);
+    printf("[PlannerBase] set tol_acc: %f\n", tol_acc);
+  }
+}
+
+template <int Dim>
+bool PlannerBase<Dim>::plan(const PlannerBase::Coord &start,
+                            const PlannerBase::Coord &goal) {
+  if (planner_verbose_) {
+    start.print("Start:");
+    goal.print("Goal:");
+
+    env_->info();
+  }
+
+  if (!env_->is_free(start.pos)) {
+    printf(ANSI_COLOR_RED "[PlannerBase] start is not free!" ANSI_COLOR_RESET
+                          "\n");
+    return false;
+  }
+
+  std::unique_ptr<GraphSearch<Dim>> planner_ptr(
+      new GraphSearch<Dim>(planner_verbose_));
+
+  // If use A*, reset the state space
+  if (!use_lpastar_)
+    ss_ptr_.reset(new MPL::StateSpace<Dim>(epsilon_));
+  else {
+    // If use LPA*, reset the state space only at the initial planning
+    if (!initialized()) {
+      if (planner_verbose_)
+        printf(ANSI_COLOR_CYAN
+               "[PlannerBase] reset planner state space!" ANSI_COLOR_RESET
+               "\n");
+      ss_ptr_.reset(new MPL::StateSpace<Dim>(epsilon_));
+    }
+  }
+
+  env_->set_goal(goal);
+
+  env_->expanded_nodes_.clear();
+  env_->expanded_edges_.clear();
+
+  ss_ptr_->dt_ = env_->get_dt();
+  if (use_lpastar_)
+    traj_cost_ = planner_ptr->LPAstar(start, env_, ss_ptr_, traj_, max_num_);
+  else
+    traj_cost_ = planner_ptr->Astar(start, env_, ss_ptr_, traj_, max_num_);
+
+  if (std::isinf(traj_cost_)) {
+    if (planner_verbose_)
+      printf(ANSI_COLOR_RED "[PlannerBase] Cannot find a traj!" ANSI_COLOR_RESET
+                            "\n");
+    return false;
+  }
+
+  return true;
+}
+
+template class PlannerBase<2>;
+template class PlannerBase<3>;
+
+}  // namespace MPL

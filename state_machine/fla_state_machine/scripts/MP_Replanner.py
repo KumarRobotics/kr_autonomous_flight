c3b0736f7d2be09c07afe1d21e431ff5428a76bb
XuRobotics <liuxu@seas.upenn.edu>
2021-04-16 22:40:53 -0400

    fix remaining issues caused by msg migration

--- state_machine/fla_state_machine/scripts/MP_Replanner.py
+++ state_machine/fla_state_machine/scripts/MP_Replanner.py
@@ -10,7 +10,6 @@ import smach_ros
 from MainStates import *
 import geometry_msgs.msg as GM
 import kr_mav_msgs.msg as QM
-import action_planner.msg as AP
 import fla_state_machine.msg as SM
 
 from Utils import *

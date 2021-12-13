464952af7115be662d73ac289188a0fb9002f39e
XuRobotics <liuxu@seas.upenn.edu>
2021-12-12 22:53:55 -0500

    adding coverage planner utils for visualizing real world tree data in RVIZ

---
+++ autonomy_core/map_plan/coverage_utils/include/utils.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <planning_ros_msgs/Path.h>
+
+#include <Eigen/StdVector>
+
+// Functions for convex_hull calculation are from
+// https://www.topcoder.com/blog/problem-of-the-week-save-the-trees/
+
+struct pt {
+  double x, y;
+};
+bool cmp(pt a, pt b);
+bool cw(pt a, pt b, pt c);
+bool ccw(pt a, pt b, pt c);
+template <int N>
+using Vecf = Eigen::Matrix<double, N, 1>;
+typedef Vecf<2> Vec2f;
+typedef Vecf<3> Vec3f;
+template <typename T>
+using vec_E = std::vector<T, Eigen::aligned_allocator<T>>;
+typedef vec_E<Vec2f> vec_Vec2f;
+typedef vec_E<Vec3f> vec_Vec3f;
+planning_ros_msgs::Path path_to_ros(const vec_Vec3f& path, double h = 0);
+void convex_hull(std::vector<pt>& a);
+std::vector<pt> PreprocessData();
---
+++ autonomy_core/map_plan/coverage_utils/src/coverage_utils.cpp
@@ -0,0 +1,55 @@
+#include <coverage_utils.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/PolygonStamped.h>
+#include <math.h>
+
+#include <boost/timer/timer.hpp>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Timer stuff
+using boost::timer::cpu_timer;
+using boost::timer::cpu_times;
+
+CoveragePlanner::CoveragePlanner() : private_node_handle_("/") {
+  initial_polygon_publisher_ =
+      private_node_handle_.advertise<geometry_msgs::PolygonStamped>(
+          "original_polygon", 1);
+  path_pub_ = private_node_handle_.advertise<planning_ros_msgs::Path>(
+      "coverage_path", 1, true);
+  all_points_pub_ = private_node_handle_.advertise<planning_ros_msgs::Path>(
+      "all_input_points", 1, true);
+}
+
+void CoveragePlanner::RunPlanner() {
+  // load all points
+  auto pt_vec = PreprocessData();
+
+  vector<pt>::iterator it;
+  vec_Vec3f all_points;
+  for (it = pt_vec.begin(); it != pt_vec.end(); it++) {
+    all_points.push_back(Vec3f(it->x, it->y, 5));
+  }
+
+  // convex hull of all points
+  convex_hull(pt_vec);
+  geometry_msgs::PolygonStamped stamped_poly;
+  stamped_poly.header.frame_id = "map";
+  stamped_poly.header.stamp = ros::Time::now();
+  for (it = pt_vec.begin(); it != pt_vec.end(); it++) {
+    geometry_msgs::Point32 point;
+    point.x = float(it->x);
+    point.y = float(it->y);
+    point.z = float(0.0);
+    stamped_poly.polygon.points.push_back(point);
+  }
+
+  auto all_points_msg = path_to_ros(all_points);
+  all_points_msg.header.frame_id = "map";
+  all_points_pub_.publish(all_points_msg);
+
+  initial_polygon_publisher_.publish(stamped_poly);
+}

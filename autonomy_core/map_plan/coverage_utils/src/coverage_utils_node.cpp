5d9c4b58b3a36d7defc684c6d5039c15005b0df1
XuRobotics <liuxu@seas.upenn.edu>
2021-12-13 17:15:03 -0500

    fix coverage utils to publish data correctly, update quadrotor launch

--- autonomy_core/map_plan/coverage_utils/src/coverage_utils_node.cpp
+++ autonomy_core/map_plan/coverage_utils/src/coverage_utils_node.cpp
@@ -4,11 +4,11 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "coverage_planner");
   ros::NodeHandle nh;
   CoveragePlanner planner;
-  ros::Rate r(0.1);
-  while (nh.ok()) {
-    planner.RunPlanner();
-    ros::spinOnce();
-    ROS_INFO("Publishing data for coverage...");
-    r.sleep();
-  }
+  ros::Rate r(1);
+  // while (nh.ok()) {
+  r.sleep();
+  planner.RunPlanner();
+  ros::spinOnce();
+  ROS_INFO("Publishing data for coverage...");
+  // }
 }

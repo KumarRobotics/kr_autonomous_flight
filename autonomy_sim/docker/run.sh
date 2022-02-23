e04d228a7db663a337b4a238cb292bb88706867d
Fernando Cladera <fcladera@fcladera.com>
2022-02-23 01:07:39 -0500

    Shellcheck bugs

--- autonomy_sim/docker/run.sh
+++ autonomy_sim/docker/run.sh
@@ -4,8 +4,8 @@
 docker run --gpus all -it --rm --network=host --name state_machine \
     --gpus all \
     --privileged \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY="$DISPLAY" \
     -e QT_X11_NO_MITSHM=1 \
-    -e XAUTHORITY=$XAUTH \
+    -e XAUTHORITY="$XAUTH" \
     kumarrobotics/autonomy:sim \
     bash

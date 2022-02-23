5b7ca4eb5a8e4692a18374dd9aa78b5263a5a18f
Fernando Cladera <fcladera@fcladera.com>
2022-02-23 00:55:04 -0500

    First sim image working

---
+++ autonomy_sim/docker/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO: add command line argument for rosmaster uri
+docker run --gpus all -it --rm --network=host --name state_machine \
+    --gpus all \
+    --privileged \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    kumarrobotics/autonomy:sim \
+    bash
